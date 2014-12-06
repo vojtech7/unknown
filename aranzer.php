863310e94f6980052948942aad4537b287280ebc
Vojtěch Novotný <vojtechnovotny7@gmail.com>
2014-12-06 01:00:06 +0100

    aranžér
    zatím nefunguje live vyhledávání u jmen

--- aranzer.php
+++ aranzer.php
@@ -1,207 +1,199 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
-<head>
-<meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="css/styl.css">
-<title>Filharmonie LiptÃ¡kov</title>
-
-
-
-<?php
-require 'Nette/loader.php';
-use Nette\Forms\Form;
-?>
-
-</head>
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/styl.css">
+    <link href="css/form.css" rel="stylesheet">
+    <meta charset="iso-8859-2">
+    <script type="text/javascript" src="netteForms.js"></script>
+    <script src="js/libs/jquery-2.1.1.js"></script>
+    <script src="js/filter.js"></script>
+    <script src="js/form.js"></script>
+    <title>Aran¾ér Filharmonie Liptákov</title>
+  </head>
 <body>
-		<?php
-			$buttons = new Form;
-			$buttons->addButton('logout', 'OdhlÃ¡sit');
-			if ($_GET["user"] == "aranzer") {
-				$buttons->addButton('find_song', 'Vyhledat skladbu');
-				$buttons->addButton('add_song', 'PÅ™idat skladbu');
-				$buttons->addButton('alter_song', 'Upravit skladbu');
-				$buttons->addButton('delete_song', 'Odstranit skladbu');
-			}
-			if ($_GET["user"] == "personalista") {
-				$buttons->addButton('find_human', 'Vyhledat zamÄ›stnance');
-				$buttons->addButton('add_human', 'PÅ™idat zamÄ›stnance');
-				$buttons->addButton('alter_human', 'Upravit zamÄ›stnance');
-				$buttons->addButton('delete_human', 'Odstranit zamÄ›stnance');
-			}
-			if ($_GET["user"] == "nastrojar") {
-				$buttons->addButton('find_instrument', 'Vyhledat zamÄ›stnance');
-				$buttons->addButton('add_instrument', 'PÅ™idat zamÄ›stnance');
-				$buttons->addButton('alter_instrument', 'Upravit zamÄ›stnance');
-				$buttons->addButton('delete_instrument', 'Odstranit zamÄ›stnance');
-			}
-		?>
-		<div id="logout" class="buttons"><?php echo $buttons['logout']->control ?></div>
 
-		<?php
-			$moznosti_aranzer = array('ID skladby', 'NÃ¡zev','DÃ©lka','ID autora' );
-			$moznosti_personalista = array('RodnÃ© ÄÃ­slo', 'JmÃ©no', 'PÅ™Ã­jmenÃ­');
-			$moznosti_nastrojar = array('VÃ½robnÃ­ ÄÃ­slo', 'Datum vÃ½roby', 'VÃ½robce', 'Datum poslednÃ­ revize', 'Datum poslednÃ­ vÃ½mÄ›ny', 'VymÄ›nÄ›no');
-			
+  <!-- uvodni inicializace -->
 
-			$select = new Form;
-		
-			$select->addText('value_1');
-			$select->addText('value_2');
-			$select->addText('value_3');
-			$select->addText('value_4');
-			$select->addText('value_5');
-			$select->addText('value_6');
-			$select->addText('value_7');
+  <?php  
+    include "connect.php";
 
-		?>
+     //získání jmen autorù pro dalsí práci
+    $sql = "select jmeno, prijmeni from Autor";
+    $autori = mysql_query($sql);
 
-		<table id="hledani">
-			<span class="nadpis" id="nadpis_vyhledavani">
-			<?php
-				switch ($_GET["user"]) {
-					case "aranzer" :
-						echo "Filtry pro vyhledÃ¡vÃ¡nÃ­ skladeb";
-						break;
-					case "personalista" :
-						echo "Filtry pro vyhledÃ¡vÃ¡nÃ­ zamÄ›stnancÅ¯";
-						break;
-					case "nastrojar" :
-						echo "Filtry pro vyhledÃ¡vÃ¡nÃ­ nÃ¡strojÅ¯";
-						break;
-					default:
-						# code...
-						break;
-				}
+    for ($i=0; $i < mysql_num_rows($autori); $i++) { 
+      $row = mysql_fetch_array($autori, MYSQL_ASSOC);
+      $seznam_jmen[$i] = $row["jmeno"]."  ".$row["prijmeni"];
+    }
 
-			?>
-			</span>
-			<tr>
-				<?php
-				$vyber = "moznosti_".$_GET["user"];
-				$count=0;
-				
-				foreach ($$vyber as $value) {
-					echo "<td>". $value ."</td>";
-					$count++;
-				}
+    $tabulka = "Skladba";
+    $nadpisy_sloupcu = array('ID skladby', 'Název', 'Délka', 'Jméno autora');
+    $nazvy_sloupcu = array('ID_skladby', 'nazev', 'delka', 'ID_autora');
+    $pk = "ID_skladby";
+    $nadpis_vysledku = "Seznam skladeb";
+    echo "<div id=logout_btn><a href='index.php'>Odhlásit se</a></div>";
+    echo '<div id="menu"><ul>';
+     // echo "<ul><li><a href='P_add_form_show()'>Pøidat zamìstnance</a></li>";
+    echo "<button onclick='P_add_form_show()'>Pøidat skladbu</button>";
+    echo "</ul><div>";
 
-				?>
-			</tr>
-			<tr>
-				<?php
-					for ($i=1; $i <= $count ; $i++) { 
-						$vypis="value_".$i;
-						echo "<td>".$select[$vypis]->control."</td>";
-					}
-				?>
+  ?>
 
-			</tr>
-		</table>
-		<div class="buttons" id="vyhledat"><?php if ($_GET["user"] == "aranzer") {
-											     	echo $buttons['find_song']->control;
-												}
-												elseif ($_GET["user"] == "personalista") {
-													echo $buttons['find_human']->control;
-												}
-												elseif ($_GET["user"] == "nastrojar") {
-													echo $buttons['find_instrument']->control;
-												}
-											 ?></div>
-	
-		<table id="prehled">
-			<span class="nadpis" id="nadpis_vysledku">
-			<?php
-				switch ($_GET["user"]) {
-					case "aranzer" :
-						echo "Seznam  skladeb";
-						break;
-					case "personalista" :
-						echo "Seznam  zamÄ›stnancÅ¯";
-						break;
-					case "nastrojar" :
-						echo "Seznam  nÃ¡strojÅ¯";
-						break;
-					default:
-						# code...
-						break;
-				}
+    <!-- <div id="logout" class="buttons"> </div> -->
 
-			?>
-			</span>
-			<tr>
-				<td class="hlavicka" id="check">VÃ½bÄ›r</td>
-				<?php
-				$vyber = "moznosti_".$_GET["user"];
-				$count=0;
-				
-				foreach ($$vyber as $value) {
-					echo "<td class=\"hlavicka\">". $value ."</td>";
-					$count++;
-				}
+    <!-- tabulka se vstupy pro hledani -->
+    <table id="hledani" class="pattern">
+    <span class="nadpis" id="nadpis_vyhledavani">Filtry pro vyhledávání skladeb</span>
+    <tr>
+    <?php
+        foreach ($nadpisy_sloupcu as $value) {
+          echo "<td>". $value ."</td>";
+        }
+        echo "</tr>";
+        echo "<tr>";
+        foreach ($nazvy_sloupcu as $value) {
+          if ($value !== "ID_autora") {
+             echo "<td> <input type=\"text\" class=\"form-control filter_". $value ."\"></td>";
+            }
+          else
+            { echo "<td><select class=\"filter_predpis form-control\">";
+              echo "<option value=\"\" selected=\"selected\"></option>";
+              foreach ($seznam_jmen as $key) {
+                echo "<option value=\"".$key."\">".$key."</option>";
+              }
+              echo "</td>";
+            }
+            
+        }
 
-				?>
-				
-			</tr>
-			<tr>
-				<td><input type="checkbox"> </td>
-				<?php
-					for ($i=1; $i <= $count ; $i++) { 
-						$vypis="value_".$i;
-						echo "<td>".$select[$vypis]->control."</td>";
-					}
-				?>
-			</tr>
+          
+        
+        echo "</tr>";
+        echo "</table>";
 
-		</table>
-		<div id="tlacitka">
-			<div class="buttons" id="add"><?php switch ($_GET["user"]) {
-													case 'aranzer':	
-														echo $buttons['add_song']->control;
-														break;
-													case 'personalista':
-														echo $buttons['add_human']->control;
-														break;
-													case 'nastrojar':
-														echo $buttons['add_instrument']->control;
-														break;
-													default:
-														# code...
-														break;
-												}
-										 ?></div>
-			<div class="buttons" id="alter"><?php switch ($_GET["user"]) {
-													case 'aranzer':	
-														echo $buttons['alter_song']->control;
-														break;
-													case 'personalista':
-														echo $buttons['alter_human']->control;
-														break;
-													case 'nastrojar':
-														echo $buttons['alter_instrument']->control;
-														break;
-													default:
-														# code...
-														break;
-												}
-										 ?></div>
-			<div class="buttons" id="delete"><?php switch ($_GET["user"]) {
-													case 'aranzer':	
-														echo $buttons['delete_song']->control;
-														break;
-													case 'personalista':
-														echo $buttons['delete_human']->control;
-														break;
-													case 'nastrojar':
-														echo $buttons['delete_instrument']->control;
-														break;
-													default:
-														# code...
-														break;
-												}
-										 ?></div>
+        //tabulka pro zobrazovani vysledku hledani
+        echo '<table id="prehled" class="data">';
+        echo '<span class="nadpis" id="nadpis_vysledku">';
+        echo $nadpis_vysledku;
+        echo "</span>";
 
-		</div>
-	
-</body>
+        echo "<tr>";
+        $count=0;
+        foreach ($nadpisy_sloupcu as $value) {
+          echo "<td class=\"hlavicka\">". $value ."</td>";
+          $count++;
+        }
+        echo "</tr>";
+
+        echo "<tr>";
+
+        //pred zobrazenim radku se provedou pripadne SQL dotazy nad tabulkou
+        //odstraneni hudebnika z tabulky
+        if(isset($_GET['delete'])) {
+          $delete_row = "DELETE FROM ".$tabulka." WHERE ".$pk.'="'.$_GET['delete'].'";';
+          $delete_success = mysql_query($delete_row);
+          if(!$delete_success) echo "nepodarilo se odstranit polozku";
+        }
+        //pridani radku do tabulky
+        if(isset($_GET["jmeno"]) and isset($_GET["prijmeni"]) and isset($_GET["rodne_cislo"])) {
+          $jmeno = $_GET["jmeno"];
+          $prijmeni = $_GET["prijmeni"];
+          $rodne_cislo = $_GET["rodne_cislo"];
+          $insert_row = "INSERT INTO ".$tabulka." VALUES (\"".$rodne_cislo."\", \"".$jmeno."\", \"".$prijmeni."\");";
+          $insert_success = mysql_query($insert_row);
+          if(!$insert_success) echo "nepodarilo se vlozit polozku";
+        }
+
+        /*tahani dat z databaze*/
+        $sql = "select * from ".$tabulka;
+        
+        $vysledek = mysql_query($sql);
+        $columns_count = count($nazvy_sloupcu);
+
+        //vykresleni radku a sloupcu s vysledky
+        //posledni sloupec se vykresluje zvlast,
+        //je slozitejsi kvuli datum z jine tabulky
+        while($row = mysql_fetch_array($vysledek)){
+          echo "<tr>";
+          for ($i=0; $i < $columns_count-1; $i++) { 
+            echo "<td class='filter_{$nazvy_sloupcu[$i]}'>{$row[$i]}</td>";
+          }
+          $dotaz = "select jmeno, prijmeni from Autor where ID_autora=".$row[3];
+          $jmeno = mysql_fetch_array(mysql_query($dotaz));
+          echo "<td class='filter_{$nazvy_sloupcu[$i]}'>".$jmeno[0]." ".$jmeno[1]."</td>";
+
+          //predam si PK do url parametru delete
+          echo "<td id=delete_btn><a href='#?page={$_GET['page']}&delete={$row[$pk]}'>Odstranit</a></td>";
+          echo "</tr>";
+        }
+
+        echo "</tr>";
+        echo "</table>";
+
+       ?>
+
+      </div>
+  <!-- formular pro pridani -->
+  <div id="P_add_form" class="abc">
+  <!-- Popup Div Starts Here -->
+  <div id="popupContact">
+  <!-- Contact Us Form -->
+  <img id="close" src="images/3.png" onclick ="P_add_form_hide()">
+  <!--
+  select {
+    background-color: #FDFBFB;
+    border: 1px #BBBBBB solid;
+    padding: 2px;
+    margin: 1px;
+    font-size: 14px;
+    color: #808080;
+  }
+  -->
+
+  <!-- vvvvvvvvvvvvv Nette Form  vvvvvvvvvvvvv -->
+  <?php
+  require 'Nette/loader.php';
+
+    use Nette\Forms\Latin2Form;
+    require_once 'Nette/Forms/Latin2Form.php';
+  ?>
+
+  <?php
+    $form = new Latin2Form;
+    $form->setAction('index.php?page=personalista.php');
+    $form->setMethod('GET');
+
+    $form->addText('jmeno', 'Jméno:')
+      ->addRule(Latin2Form::FILLED, 'Zadejte jméno');
+    $form->addText('prijmeni', 'Pøíjmení:')
+      ->addRule(Latin2Form::FILLED, 'Zadejte pøíjmení');
+    $form->addText('rodne_cislo', 'Rodné èíslo:')
+      ->addRule(Latin2Form::FILLED, 'Zadejte rodné èíslo');
+    $form->addSubmit('send', 'Pøidat');
+  ?>
+
+  <script src="netteForms.js"></script>
+  <style> .required label { color: maroon } </style>
+
+  <?php
+  echo $form; // vykresli formular
+
+  $sub1 = $form->addContainer('first');
+
+  if ($form->isSuccess()) {
+    echo 'Formuláø byl správnì vyplnìn a odeslán';
+      $values = $form->getValues();
+    dump($values);
+  }
+
+  ?>
+  <!-- ^^^^^^^^^^^^^ Nette Latin2Form  ^^^^^^^^^^^^^ -->
+
+</div>
+<!-- Popup Div Ends Here -->
+</div>
+<!-- Display Popup Button -->
+<!-- <button id="popup" onclick="P_add_form_show()">Popup</button> -->
+    
+  </body>
 </html>
